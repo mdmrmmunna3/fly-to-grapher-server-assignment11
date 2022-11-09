274adf5a1ab3b9bdb2d5b1e8f0b3ccb2a6092f2c
mdmrmmunna3 <mdmunnaking1209@gmail.com>
2022-11-09 22:25:45 +0600

    add service data in mongodb and show local host ui

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -7,6 +8,29 @@ const port = process.env.PORT || 5000;
 // middleWares 
 app.use(cors());
 app.use(express.json());
+
+//photographer, services
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.le2w9sh.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run () {
+    try {
+        const photographerCollection = client.db('photographer').collection('services');
+
+        app.get('/services', async(req, res) => {
+            const query  = {};
+            const cursor = photographerCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err));
 
 app.get('/', (req, res) => {
     res.send('Fly-to-grapher-studio server running')
